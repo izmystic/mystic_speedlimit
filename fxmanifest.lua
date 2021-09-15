d1ca870ea461cd43401f0c644ead3471d1d284ef
izMystic <mystic@izmystic.dev>
2021-09-15 03:04:36 -0400

    Speedlimit sign

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,22 @@
 -- Resource Metadata
-fx_version 'bodacious'
-games { 'gta5' }
+fx_version 'cerulean'
+game 'gta5'
 
 author 'Linden'
 description "Based on Big Yoda's posted-speedlimit script."
 version '1.0.0'
 
 -- What to run
-client_scripts {
-    'cl_speed.lua'
-}
+client_script 'cl_speed.lua'
 
-ui_page('html/index.html')
+ui_page 'html/index.html'
 
-files({
+files{
     'html/index.html',
     'html/app.js',
     'html/style.css',
-})
+    'html/speedlimit.png'
+}
 
 export "getSpeed"
 export "getStreet"
