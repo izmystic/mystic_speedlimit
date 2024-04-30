d252f964488146b69600550196972b887ad466e1
izmystic <mystic@izmystic.dev>
2024-04-30 03:20:19 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,18 +2,18 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-shared_script '@ox_lib/init.lua'
-client_scripts {
-  'config.lua',
-  'client.lua'
-}
+author 'mystic'
+description 'speedlimit display for fivem'
+
+shared_scripts { '@ox_lib/init.lua', 'config.lua' }
+client_script 'client.lua'
 
 ui_page 'html/index.html'
 
 files {
-  'html/index.html',
-  'html/script.js',
-  'html/style.css',
-  'html/fonts/HWYGWDE.woff',
-  'html/fonts/SaaSeriesEDOT.woff'
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/fonts/HWYGWDE.woff',
+    'html/fonts/SaaSeriesEDOT.woff'
 }
